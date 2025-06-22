aadcaf7c5536ad13fc525b12e9ac0e5a40a775cd
Gabriel Martins <gabriel.martins.1@academico.ifpb.edu.br>
2025-06-22 18:43:16 -0300

    pop-ups

---
+++ Nucleo/static/Nucleo/js/script.js
@@ -0,0 +1,31 @@
+function Pesquisar(){
+    elemento = document.getElementById("pesquisa")
+    const lista_de_cidades = [
+        {'nome':'Picuí'},
+        {'nome':'Frei martinho'},
+        {'nome':'Barauna'},
+        {'nome':'Barra de Santa Rosa'},
+        {'nome':'Cuité'},
+        {'nome':'Cubati'},
+        {'nome':'Nova Palmeira'},
+        {'nome':'Nova Floresta'},
+        {'nome':'Sossego'},
+        {'nome':'São Vincente'},
+        {'nome':'Solidade'},
+        {'nome':'Damião'},
+        {'nome':'Algodão de Jnadaíra'},
+        {'nome':'Pedra Lavrada'}
+    ]
+    for(let i in lista_de_cidades){
+        if( i == elemento){
+
+        }
+    }
+}
+function mostrarContrato20() {
+  document.getElementById("contrato20").style.display = "block";
+}
+
+function fecharContrato20() {
+  document.getElementById("contrato20").style.display = "none";
+}
